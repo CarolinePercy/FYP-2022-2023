338288fe180bc22782b170d23e57e378359d3db3
Caroline Percy <lineypercy@me.com>
2023-03-16 16:03:11 +0000

    WIP - need for later

---
+++ Other/Cityscape/README_panopticParts.md
@@ -0,0 +1,29 @@
+
+README and scripts for Cityscapes Panoptic Parts
+------------------------------------------------
+
+Cityscapes Panoptic Parts extends the original Cityscapes annotations (train fine and val sets) with part-level semantics. It contains manual annotations for 23 part-level classes that correspond to 5 scene-level things classes from the original Cityscapes dataset.
+
+README and various scripts can be found in the following git repository:
+https://github.com/pmeletis/panoptic_parts
+
+The original git repository of Cityscapes is:
+https://github.com/mcordts/cityscapesScripts
+
+
+Citation
+--------
+
+If you use the Cityscapes Panoptic Parts annotations and code in your research, please cite both publications:
+- M. Cordts, M. Omran, S. Ramos, T. Rehfeld, M. Enzweiler, R. Benenson, U. Franke, S. Roth, and B. Schiele, "The Cityscapes Dataset for Semantic Urban Scene Understanding," in CVPR 2016.
+- P. Meletis, X. Wen, C. Lu, D. de Geus, and G. Dubbelman, "Cityscapes-Panoptic-Parts and PASCAL-Panoptic-Parts datasets for Scene Understanding", Technical report in arXiv 2004.07944, 2020.
+
+
+Contact
+-------
+
+Panagiotis Meletis: p.meletis@outlook.com
+Vincent (Xiaoxiao) Wen: wenxx10@gmail.com
+
+The Cityscapes Panoptic Parts team, Mobile Perception Systems lab of Eindhoven University of Technology
+
---
+++ Other/Example MLP/ConsoleMLP/ConsoleMLP/ConsoleMLP.cpp
@@ -0,0 +1,86 @@
+// Example MLP network using the MNIST data.
+// Source: https://github.com/pimlu/cppnn
+//
+// ConsoleMLP.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "MLP.h"
+#include "mnist.h"
+
+#include <iostream>
+#include <vector>
+#include <initializer_list>
+#include <conio.h>	// _getch()
+
+//rand
+#include <cstdlib>
+#include <ctime> 
+
+using namespace Eigen;	//http://eigen.tuxfamily.org/index.php?title=Main_Page
+using namespace std;
+
+
+template <typename T>
+void print(const char* name, const vector<T> &v) {
+	cout << name << ":" << endl;
+	for (size_t i = 0; i<v.size(); i++) {
+		cout << i << ":" << endl;
+		cout << v[i] << endl;
+	}
+}
+template <typename T>
+void print(const char* name, const T &t) {
+	cout << name << ":" << endl;
+	cout << t << endl;
+}
+
+void sample(vector<MLP::sample> &trainset, initializer_list<num> in_, initializer_list<num> out_) {
+	ArrayXn in = Map<const ArrayXn>(in_.begin(), in_.size());
+	ArrayXn out = Map<const ArrayXn>(out_.begin(), out_.size());
+	MLP::sample goal(in, out);
+	trainset.push_back(goal);
+}
+
+void tryall(MLP net) {
+	for (int i = 0; i<4; i++) {
+		ArrayXn input(2);
+		input << i / 2, i % 2;
+		print("input", input);
+		print("output", net.eval(input));
+	}
+}
+
+int doxor() {
+	ArrayXi layers(3);
+	layers << 2, 6, 1;
+
+	MLP net(layers, 1);
+
+	vector<MLP::sample> tset;
+	sample(tset, { 0,0 }, { 0.1 });
+	sample(tset, { 0,1 }, { 0.9 });
+	sample(tset, { 1,0 }, { 0.9 });
+	sample(tset, { 1,1 }, { 0.1 });
+
+	print("WEIGHTS", net.weights);
+
+	tryall(net);
+
+	for (int i = 0; i<1000; i++) net.rprop(tset, 0.1, 1.2, 0.5);
+	//for(int i=0; i<10000; i++) net.train(tset, 0.1);
+
+	print("WEIGHTS", net.weights);
+
+	tryall(net);
+	return 0;
+}
+int main(int argc, char **argv) 
+{
+	srand((unsigned int)time(0));
+	mnist(argc, argv);
+	_getch(); // wait until any key is pressed
+
+}
+
